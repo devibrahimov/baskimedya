73514ae8337dcf6fc08e33143604bdc280773a6a
devibrahimov <dev.ibrahimov@gmail.com>
2020-08-21 01:48:06 +0300

    cretatw basket migration

---
+++ database/migrations/2020_08_20_221105__create_table_basket_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBasketProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basket_products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basket_products');
+    }
+}
